48d39365e387037bf87ba4f6e453481b20167d26
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-11-03 21:19:16 +0700

    config theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Topbar from './components/Topbar';
 import DiscoverFeature from './features/Discover/index';
 import Footer from './components/Footer';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Topbar />
       <DiscoverFeature />
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
